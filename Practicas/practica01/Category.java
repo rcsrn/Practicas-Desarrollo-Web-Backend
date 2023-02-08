ed207dc4e0998b9e190eae8f5f3e55472085f1fe
nsgbanks@ciencias.unam.mx <nsgbanks@ciencias.unam.mx>
2023-02-07 22:20:16 -0600

    First version of program. It is necessary to override the toString method in Category

--- Practicas/practica01/Category.java
+++ Practicas/practica01/Category.java
@@ -1,33 +1,47 @@
-import java.util.LinkedList; 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.lang.IllegalArgumentException;
 
 public class Category {
 
     private Integer category_id;
     private String category;
     private String acronym;
-    private static LinkedList<Categories> categories;
+    private static LinkedList<Category> categories = new LinkedList<Category>();
     
-
     public Category(Integer category_id, String category, String acronym) {
 	this.category_id = category_id;
 	this.category = category;
-	this.acronym;
+	this.acronym = acronym;
     }
     
-    public static void createCategory(Category categ) {
-       
+    public static void createCategory(Category newCategory) {
+	for (Category category : categories)
+	    if (category.category_id.intValue() == category.category_id.intValue())
+		throw new IllegalArgumentException();
+	categories.addLast(newCategory);
     }
 
     public static String getCategories() {
 	return categories.toString();
     }
-    
-    public static void getCategory(int category_id) {
-	
+
+    public static boolean listIsEmpty() {
+	return categories.size() == 0;
     }
     
-    public static void deleteCategory(int category_id) {
-	
+    public static Category getCategory(int category_id) {
+	for (Category category : categories) 
+	    if (category.category_id == category_id)
+		return category;
+	throw new NoSuchElementException();
+    }
+    
+    public static void deleteCategory(Integer category_id) {
+	for (Category category : categories)
+	    if (category.category_id.intValue() == category_id.intValue())
+		categories.remove(category);
+	throw new NoSuchElementException();
     }
     
 }
--- Practicas/practica01/Practica1.java
+++ Practicas/practica01/Practica1.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.NoSuchElementException;
+import java.lang.IllegalArgumentException;
 
 public class Practica1 {
     
@@ -9,44 +11,74 @@ public class Practica1 {
 	    createCategoryAction();
 	} else if (action == 2) {
 	    getCategoryListAction();
+	} else if (action == 3) {
+	    findCategoryAction();
+	} else if (action == 4) {
+	    removeCategoryAction();
 	} else {
-	    queryCategoryAction();
+	    use();
 	}
     }
     
     private static void createCategoryAction() {
 	Scanner scan  = new Scanner(System.in);
 	
-	System.out.println("Ingrese el id");
-	String id = scan.nextLine();
+	System.out.println("Ingrese el id.");
+	Integer categoryId = scan.nextInt();
 	
-	System.out.println("Ingrese la categoria");
+	System.out.println("Ingrese la categoria.");
 	String category = scan.nextLine();
 	
-	System.out.println("Ingrese el acronimo");
+	System.out.println("Ingrese el acronimo.");
 	String acronym = scan.nextLine();
-	
-	Category.createCategory(new Category(id, category, acronym));
+
+	try {
+	Category.createCategory(new Category(categoryId, category, acronym));
+	} catch (IllegalArgumentException iae) {
+	    System.out.println("El id de la categoria ingresado ya existe.");
+	}
     }
 
     private static void getCategoryListAction() {
-	System.out.println(Category.getCategories());
+	if (Category.listIsEmpty()) 
+	    System.out.println("No existen categorias registradas");
+	else System.out.println(Category.getCategories());
     }
 
-    private static void queryCategoryAction() {
-	
+    private static void findCategoryAction() {
+	System.out.println("Ingrese el id de la categoria a buscar.");
+	Integer categoryId = reader.nextInt();
+
+	try {
+	    Category category = Category.getCategory(categoryId);
+	    System.out.println(category.toString());
+	} catch (NoSuchElementException nsee) {
+	    System.out.println("No existe una categoria con el id ingresado.");
+	} 
+    }
+
+    private static void removeCategoryAction() {
+	System.out.println("Ingrese el id de la categoria a eliminar.");
+	Integer categoryId = reader.nextInt();
+
+	try {
+	    Category.deleteCategory(categoryId);
+	} catch (NoSuchElementException nsee) {
+	    System.out.println("No existe una categoria con el id ingresado.");
+	}
     }
     
-    
     private static void showInstructions() {
-	String s1 = "Ingrese 1 si desea crear alguna categoria\n";
-	String s2 = "Ingrese 2 si desea ver la lista de categorias existentes\n";
-	String s3 = "Ingrese 3 si desea consular alguna categoria\n";
+	String s1 = "Ingrese 1 si desea crear alguna categoria.\n";
+	String s2 = "Ingrese 2 si desea ver la lista de categorias existentes.\n";
+	String s3 = "Ingrese 3 si desea consular alguna categoria.\n";
 	
 	System.out.println(s1 + s2 + s3);
     }
 
-    
+    private static void use() {
+	System.out.println("Ingrese una opcion valida.");
+    }
     
     public static void main(String[] args) {
 	reader = new Scanner(System.in);
@@ -56,15 +88,3 @@ public class Practica1 {
 	}
     }   
 }
-
-// Scanner scan  = new Scanner(System.in);
-	
-// 	System.out.println("Ingrese el id");
-// 	String id = scan.nextLine();
-	
-// 	System.out.println("Ingrese la categoria");
-// 	String category = scan.nextLine();
-
-// 	System.out.println("Ingrese el acronimo");
-// 	String acronym = scan.nextLine();
-
