567494bbc46c51a8b6feab7b67666f77990d6aa6
casarin <nsgbanks@ciencias.unam.mx>
2023-03-03 17:15:48 -0600

    refactor: createCategory

--- Practicas/practica03/product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ Practicas/practica03/product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -49,9 +49,21 @@ public class CtrlCategory {
           category1.setCategoryId(1);
           category1.setCategory("Línea Blanca");
           category1.setAcronym("LB");
-          
           category1.setStatus(1);
-          return new ResponseEntity<>(category1, HttpStatus.OK);
+
+          Category category2 = new Category();
+          category2.setCategoryId(2);
+          category2.setCategory("Electrónica");
+          category2.setAcronym("Electr");
+          category2.setStatus(2);
+
+          if (category_id == 1) {
+               return new ResponseEntity<>(category1, HttpStatus.OK);
+          } else if (category_id == 1) {
+               return new ResponseEntity<>(category2, HttpStatus.OK);
+          }
+          
+          return new ResponseEntity<>(null, HttpStatus.OK);
      }   
 
      @PostMapping
@@ -61,6 +73,16 @@ public class CtrlCategory {
                message = bindingResult.getAllErrors().get(0).getDefaultMessage();
                return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
           }
+
+          String newCategory = category.getCategory();
+          String newAcronym = category.getAcronym();
+
+          if (newCategory.equals("Línea Blanca") || newCategory.equals("Electrónica")) {
+               return new ResponseEntity<>("category already exist", HttpStatus.OK);     
+          } else if (newAcronym.equals("LB") || newAcronym.equals("Electr")) {
+               return new ResponseEntity<>("category already exist", HttpStatus.OK);     
+          }
+          
           message = "category created";
           return new ResponseEntity<>(message, HttpStatus.OK);
      }
