6996f30ea7b47bc5f8891d34b54a1202f27f0183
casarin <nsgbanks@ciencias.unam.mx>
2023-03-05 22:58:19 -0600

    autowired at CtrlCategory

--- Practicas/practica04/product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ Practicas/practica04/product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -1,26 +1,32 @@
 package com.product.api.controller;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.Valid;
 
 import com.product.api.entity.Category;
+import com.product.api.service.SvcCategory;
 
 @RestController
 @RequestMapping("/category")
 public class CtrlCategory {
+
+     @Autowired
+     SvcCategory svc;
 
      @GetMapping
      public ResponseEntity<List<Category>> getCategories() {
