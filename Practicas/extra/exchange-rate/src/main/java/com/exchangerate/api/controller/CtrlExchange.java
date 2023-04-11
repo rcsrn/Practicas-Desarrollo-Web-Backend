a0125bf16c1cbd18816abad0058f08bfdb21c647
casarin <nsgbanks@ciencias.unam.mx>
2023-04-10 21:03:01 -0600

    RestTemplate obtains the information from the ApiRest we want to consume

--- Practicas/extra/exchange-rate/src/main/java/com/exchangerate/api/controller/CtrlExchange.java
+++ Practicas/extra/exchange-rate/src/main/java/com/exchangerate/api/controller/CtrlExchange.java
@@ -2,8 +2,11 @@ package com.exchangerate.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import com.exchangerate.api.dto.ApiResponse;
 
 
@@ -12,8 +15,11 @@ import com.exchangerate.api.dto.ApiResponse;
 public class CtrlExchange {
     
 
+
     @GetMapping("/{currency}")
-    public ResponseEntity<ApiResponse> getRate() {
-        return null;
+    public ResponseEntity<String> getRate(@PathVariable String currency) {
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> response = template.getForEntity("https://api.coincap.io/v2/rates/" + currency, String.class);
+        return response;
     }
 }
