ffea5dcb8b698a6b97f35079ca6c58e5e128cf84
casarin <nsgbanks@ciencias.unam.mx>
2023-04-16 21:44:18 -0600

    Adding product image entity

---
+++ Practicas/practica06/product/src/main/java/com/product/api/entity/ProductImage.java
@@ -0,0 +1,67 @@
+package com.product.api.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "product image")
+public class ProductImage {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_image_id")
+    private Integer product_image_id;   
+
+    @NotNull
+    @Column(name = "product_id")
+    private Integer product_id;
+    
+    @NotNull
+    @Column(name = "image")
+    private String image;
+
+    @NotNull
+    @Column(name = "status")
+    private Integer status;
+
+    public ProductImage() {
+        this.image = "";
+    }
+
+    public void setProductImageId(Integer product_image_id) {
+        this.product_image_id = product_image_id;
+    }
+
+    public Integer getProductImageId() {
+        return product_image_id;
+    }
+
+    public void setProductId(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getProductId() {
+        return product_id;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+}
